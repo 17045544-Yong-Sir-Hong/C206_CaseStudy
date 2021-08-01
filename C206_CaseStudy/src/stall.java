93a5cb00996ef232f42e8bbd4bc1a7db5f34f3b4
20007233 <20007233@LAPTOP-ERAD8JEU>
2021-08-01 15:50:23 +0800

    created stall class

---
+++ C206_CaseStudy/src/stall.java
@@ -0,0 +1,32 @@
+import java.util.Date;
+
+public class stall {
+	private String stallName ;
+	private Date date;
+	
+	public stall(String stallName, String string) {
+		
+		this.stallName= stallName;
+		this.date = date;
+		
+		
+	}
+
+	public String getStallName() {
+		return stallName;
+	}
+
+	public void setStallName(String stallName) {
+		this.stallName = stallName;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+
+}
