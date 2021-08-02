b6f0a3be07af76db1718747fbd1a34af852c67be
Yong Sir Hong <Yong Sir Hong@Ninazu-DESKTOP-LN9MCSC>
2021-08-02 21:37:48 +0800

    done Junit,tAddPurchasesOrder, tViewPurchasesOrder,tdeletePurchasesOrder

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -201,15 +201,19 @@ public class C206_CaseStudyTest {
 	@Test
 	public void testViewPurchasesOrder()
 	{
-		
+		//Given an empty list, after adding 1 item, the size of the list is 1 - normal
+		//The item just added is as same as the first item of the list
 		PurchaseOrdersList.add(purchaseOrder1);
 		assertEquals("Check that PurchaseOrder arraylist size is 1", 1, PurchaseOrdersList.size());
 		assertSame("Check that PurchaseOrder is added", purchaseOrder1, PurchaseOrdersList.get(0));
 		
+		//Add another item. test The size of the list is 2? -normal
+		//The item just added is as same as the second item of the list
 		PurchaseOrdersList.add(purchaseOrder2);
 		assertEquals("Check that PurchaseOrder arraylist size is 2", 2, PurchaseOrdersList.size());
 		assertSame("Check that PurchaseOrder is added", purchaseOrder2, PurchaseOrdersList.get(1));
 		
+		//Test PurchaseOrdersList can view purchases order
 		C206_CaseStudy.ViewPurchasesOrder(PurchaseOrdersList);
 	}
 	
